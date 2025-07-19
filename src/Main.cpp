07bb8596b810df1d3dd7c665f34968051245aa29
Spawn <spwn02.dev@gmail.com>
2025-07-20 01:37:50 +0300

    Created RGB square effect.

--- src/Main.cpp
+++ src/Main.cpp
@@ -62,7 +62,7 @@ static unsigned int compileShader(unsigned int type, const std::string& source)
     {
         int length;
         glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        char* message = (char*)alloca(length * sizeof(char));
+        char* message = (char*)malloc(length * sizeof(char));
         glGetShaderInfoLog(id, length, &length, message);
         std::cout << "Failed to compile "
             << (type == GL_VERTEX_SHADER ? "vertex" : "fragment")
@@ -98,12 +98,16 @@ int main()
 
     /* Initialize the library */
     if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
         return -1;
+    }
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Vevi", NULL, NULL);
+    window = glfwCreateWindow(800, 800, "Vevi", NULL, NULL);
     if (!window)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
@@ -111,35 +115,111 @@ int main()
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(1);
+
     if (glewInit() != GLEW_OK)
         std::cout << "Error" << std::endl;
 
-    float positions[6] = {
-        -0.5f, -0.5,
-        0.0f, 0.5f,
-        0.5f, -0.5f
+    float positions[] = {
+        -0.5f, -0.5f,
+        0.5f, -0.5f,
+        0.5f, 0.5f,
+        -0.5f, 0.5f,
     };
 
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0,
+    };
+    
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 2 * 4 * sizeof(float), positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_TRUE, sizeof(float) * 2, 0);
 
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+
     ShaderProgramSource source = parseShader("../../res/shaders/basic.shader");
     unsigned int shader = createShader(source.vertexSource, source.fragmentSource);
     glUseProgram(shader);
-    
-     /* Loop until the user closes the window */
+
+    int location = glGetUniformLocation(shader, "u_color");
+    glUniform4f(location, 0, 0, 0, 0);
+
+    struct RGB {
+        float red;
+        float green;
+        float blue;
+    };
+
+    RGB rgb = { 0, 0, 0 };
+    float incrementVal = 0.05f;
+    float increment = incrementVal;
+    char currentColor = 'r';
+
+    float highThres = 1.0f;
+    float lowThres = 0.3f;
+
+    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        if (rgb.red >= highThres && rgb.blue >= highThres)
+        {
+            increment = -incrementVal;
+            currentColor = 'r';
+        }
+        else if (rgb.red <= lowThres && rgb.blue <= lowThres)
+        {
+            increment = incrementVal;
+            currentColor = 'r';
+        }
+        if (rgb.red <= lowThres && rgb.green >= highThres)
+        {
+            increment = -incrementVal;
+            currentColor = 'g';
+        }
+        else if (rgb.red >= highThres && rgb.green <= lowThres)
+        {
+            increment = incrementVal;
+            currentColor = 'g';
+        }
+        if (rgb.green <= lowThres && rgb.blue >= highThres)
+        {
+            increment = -incrementVal;
+            currentColor = 'b';
+        }
+        else if (rgb.green >= highThres && rgb.blue <= lowThres)
+        {
+            increment = incrementVal;
+            currentColor = 'b';
+        }
 
+        switch (currentColor)
+        {
+        case 'r':
+                rgb.red += increment;
+                break;
+        case 'g':
+            rgb.green += increment;
+            break;
+        case 'b':
+            rgb.blue += increment;
+            break;
+        }
+
+        glUniform4f(location, rgb.red, rgb.green, rgb.blue, 1);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
@@ -147,8 +227,7 @@ int main()
         glfwPollEvents();
     }
 
-    // glDeleteShader(shader);
+    glDeleteProgram(shader);
 
     glfwTerminate();
-    return 0;
 }
